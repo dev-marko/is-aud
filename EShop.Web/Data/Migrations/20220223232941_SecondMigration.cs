3ed01566b08a38f83aed074ed0fa8de0495c555b
dev-marko <markospasenovski00@gmail.com>
2022-02-24 00:42:07 +0100

    Add project files.

---
+++ EShop.Web/Data/Migrations/20220223232941_SecondMigration.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EShop.Web.Data.Migrations
+{
+    public partial class SecondMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ProductInShoppingCarts_Products_ProductId",
+                table: "ProductInShoppingCarts");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_ProductInShoppingCarts_ShoppingCarts_ShoppingCartId",
+                table: "ProductInShoppingCarts");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ProductName",
+                table: "Products",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ProductDescription",
+                table: "Products",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ProductInShoppingCarts_ShoppingCarts_ProductId",
+                table: "ProductInShoppingCarts",
+                column: "ProductId",
+                principalTable: "ShoppingCarts",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ProductInShoppingCarts_Products_ShoppingCartId",
+                table: "ProductInShoppingCarts",
+                column: "ShoppingCartId",
+                principalTable: "Products",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ProductInShoppingCarts_ShoppingCarts_ProductId",
+                table: "ProductInShoppingCarts");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_ProductInShoppingCarts_Products_ShoppingCartId",
+                table: "ProductInShoppingCarts");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ProductName",
+                table: "Products",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ProductDescription",
+                table: "Products",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ProductInShoppingCarts_Products_ProductId",
+                table: "ProductInShoppingCarts",
+                column: "ProductId",
+                principalTable: "Products",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ProductInShoppingCarts_ShoppingCarts_ShoppingCartId",
+                table: "ProductInShoppingCarts",
+                column: "ShoppingCartId",
+                principalTable: "ShoppingCarts",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
